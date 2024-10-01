58e9de9979c988bf236d49e16666a4fde4c81e8d
SebastianGaleano20 <sebascarp9@gmail.com>
2024-09-30 21:52:38 -0300

    Foldes in public added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-    devToolbar: {
-        enabled: false
-      }
+  devToolbar: {
+    enabled: false
+  },
+  integrations: [react()]
 });
