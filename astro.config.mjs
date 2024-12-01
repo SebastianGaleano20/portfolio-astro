aae3b681e4ddfe3c872a469aa51626bc0469792a
SebastianGaleano20 <sebascarp9@gmail.com>
2024-11-30 22:27:47 -0300

    Card component change - Class with tailwindCSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   devToolbar: {
     enabled: false,
   },
-  integrations: [react()],
+  integrations: [react(), tailwind()],
 });
