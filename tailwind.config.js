027ac0ca09ea7d60686e83182ec8129737b15b34
SebastianGaleano20 <sebascarp9@gmail.com>
2024-12-03 22:21:32 -0300

    Components configurated of dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: "class",
   content: [
     "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
     "./node_modules/flowbite/**/*.js",
   ],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkBg: "rgb(7, 24, 58)",
+        darkGradientStart: "rgb(13, 31, 66)",
+        darkGradientEnd: "rgb(3, 3, 31)",
+        darkText: "#fafafa",
+      },
+      backgroundImage: {
+        "dark-gradient":
+          "radial-gradient(circle, rgb(13, 31, 66) 0%, rgb(3, 3, 31) 100%)",
+      },
+    },
   },
   plugins: [require("flowbite/plugin")],
 };
